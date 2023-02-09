e6a7d8f4fe79aaff1d7415cd68af2b81e54a8401
leonardo falango <leo_falango@hotmail.com>
2023-02-09 14:36:57 -0300

    add white screen

--- assembler/code.asm
+++ assembler/code.asm
@@ -1,31 +1,17 @@
-white:
+setting4096reg0:
     mov     $0, 128
     mov     $1, 5
     lsh     $0, $1
-    mov     $2, 255
-    mov     $3, 0
-    mov     [$0], $2
     mov     $10, 0
     mov     $11, 255
-    jump    whiter
-whiter:
+whiteRow:
+    mov     $1, 255
+    mov     $2, 8
+    lsh     $1, $2
+    mov     $2, 255
+    add     $1, $2
+    mov     [$0], $1
+loop:
     inc     $0
-    inc     $10
-    mov     [$0], $2
-    cmp     $10, $11
-    je      black
-    jump    white
-black:
-    mov     $0, 128
-    mov     $1, 5
-    lsh     $0, $1
-    mov     [$0], $3
-    mov     $10, 0
-    jump    blacker
-blacker:
-    inc     $0
-    inc     $10
-    mov     [$0], $3
-    cmp     $10, $11
-    je      white
-    jump    blacker
+    mov     [$0], $1
+    jump    loop
